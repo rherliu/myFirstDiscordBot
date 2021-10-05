d357db6a9d1c5698e34a652525fbf75f8f12411f
RH <87567589+rherliu@users.noreply.github.com>
2021-10-05 17:20:41 +0800

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import os
-import discord
+# import discord
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -17,36 +17,23 @@ async def on_ready():
 #基礎四則運算：add(+), sub(-), mul(*), div(/)
 @bot.command()
 async def add(self, a: float, b: float):
-    await self.send('和: '+ str(a+b))
-#     await self.send(a+b)
+    await self.send('sum: '+ str(a+b))
+
 @bot.command()
 async def sub(self, a: float, b: float):
-    await self.send('差:')
-#     await self.send(a-b)
+    await self.send('difference:')
+
 @bot.command()
 async def mul(self, a: float, b: float):
-    await self.send('積:')
-#     await self.send(a*b)
+    await self.send('product:')
+
 @bot.command()
 async def div(self, a: float, b: float):
-    await self.send('商:')
-#     await self.send(a/b)
+    await self.send('quotient:')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
 
-#影片佇列
-@bot.command()
-async def song(self, x:str):
-    video={'1': 'https://www.youtube.com/watch?v=eblocB9LzsA',
-           '2': 'https://www.youtube.com/watch?v=vH-sEWbPanU'}
-    if x in video:
-        await self.send('第' + x + '支影片: ' + video[x])
-    elif x == 'all':
-        await self.send(video)
-
-
-# bot.run(TOKEN)
 if __name__ == "__main__":
     bot.run(TOKEN)
